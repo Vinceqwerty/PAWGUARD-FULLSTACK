2f307b9f04141f87454f23db135efaf2ebccd741
ifnaticx <abcede.henz7@gmail.com>
2026-03-10 07:14:41 +0000

    N

--- frontend/pawguard/src/App.jsx
+++ frontend/pawguard/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import Dogs from "./pages/Dogs";
@@ -9,18 +8,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<LoginPage />} />
-
         <Route path="/dashboard" element={<Dashboard />} />
-
         <Route path="/dogs" element={<Dogs />} />
-
         <Route path="/health" element={<HealthRecords />} />
-
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;

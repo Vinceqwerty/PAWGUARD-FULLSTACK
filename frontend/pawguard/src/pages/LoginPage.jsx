cb1b40aa4245ccb7429fd3f99760a44bef8f5145
DagaKendrick <daga.kendricksimon@gmail.com>
2026-03-12 17:34:48 +0000

    Updated LoginPage Styles

--- frontend/pawguard/src/pages/LoginPage.jsx
+++ frontend/pawguard/src/pages/LoginPage.jsx
@@ -1,34 +1,43 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/login.css";
 
 export default function LoginPage() {
   const [username,setUsername] = useState("");
   const [password,setPassword] = useState("");
   const navigate = useNavigate();
+
   const login = () => {
     if(username && password){
       localStorage.setItem("user","admin");
       navigate("/dashboard");
-    }
-    else{
+    } else {
       alert("Enter username and password");
     }
   };
 
   return(
-    <div>
-      <h2>PawGuard Login</h2>
-      <input
-      placeholder="Username"
-      onChange={(e)=>setUsername(e.target.value)}
-      />
-      <input
-      type="password"
-      placeholder="Password"
-      onChange={(e)=>setPassword(e.target.value)}
-      />
-      <button onClick={login}>Login</button>
+    <div className="login-page">
+      <header className="login-header">
+        <h1>PawGuard</h1>
+        <p>Dog Health Monitoring System</p>
+      </header>
 
-    </div>
+      <main className="login-main">
+        <div className="login-section">
+          <h2>Sign In to your Account!</h2>
+          <div className="form-group">
+            <input placeholder="Username" onChange={(e)=>setUsername(e.target.value)}/>
+          </div>
+
+          <div className="form-group">
+            <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+          </div>
+          <div className="button-container">
+            <button className="login-button" onClick={login}>Login</button>
+          </div>
+        </div>
+       </main>
+      </div>
   );
 }
